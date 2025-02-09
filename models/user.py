6e62788617e7e091f94ac34639ec26385b46010b
mojitap <ta714kadvance@gmail.com>
2025-02-09 17:50:54 +0900

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,9 +1,11 @@
-from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
