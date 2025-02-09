d6fad810e77fb51e62d7446806122ecf866ebdd2
mojitap <ta714kadvance@gmail.com>
2025-02-09 17:40:03 +0900

    Update user.py

--- models/user.py
+++ models/user.py
@@ -7,6 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
         return f'<User {self.username}>'
