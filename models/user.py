d96cc1362c23f0ed375b381794a7f7880104e1c0
mojitap <ta714kadvance@gmail.com>
2025-02-09 17:49:12 +0900

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,13 +1,13 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    __tablename__ = 'users'
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f"<User {self.username}>"
