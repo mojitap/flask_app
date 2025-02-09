3627f422b357a8bc74b725d85327d42030824f5b
mojitap <ta714kadvance@gmail.com>
2025-02-09 19:51:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,52 @@ def load_user(user_id):
     """ユーザーIDからユーザーをロードする関数"""
     return User.query.get(int(user_id))  # user_idでDBからユーザーを取得
 
+@app.route("/login/google")
+def login_google():
+    redirect_uri = url_for("authorize_google", _external=True)
+    return oauth.google.authorize_redirect(redirect_uri)
+
+@app.route("/authorize/google")
+def authorize_google():
+    token = oauth.google.authorize_access_token()
+    user_info = oauth.google.get("https://www.googleapis.com/oauth2/v3/userinfo").json()
+    user = User(id=user_info["email"])
+    login_user(user)
+    return redirect("/")
+
+@app.route('/login/twitter')
+def login_twitter():
+    twitter = OAuth1Session(
+        client_key=os.getenv('TWITTER_API_KEY'),
+        client_secret=os.getenv('TWITTER_API_SECRET'),
+        callback_uri=url_for('authorize_twitter', _external=True)
+    )
+    request_token_url = "https://api.twitter.com/oauth/request_token"
+    response = twitter.fetch_request_token(request_token_url)
+    oauth_token = response.get('oauth_token')
+    redirect_url = f"https://api.twitter.com/oauth/authorize?oauth_token={oauth_token}"
+    return redirect(redirect_url)
+
+@app.route('/authorize/twitter')
+def authorize_twitter():
+    twitter = OAuth1Session(
+        client_key=os.getenv('TWITTER_API_KEY'),
+        client_secret=os.getenv('TWITTER_API_SECRET'),
+        resource_owner_key=request.args.get('oauth_token'),
+        verifier=request.args.get('oauth_verifier')
+    )
+    access_token_url = "https://api.twitter.com/oauth/access_token"
+    tokens = twitter.fetch_access_token(access_token_url)
+    
+    # ユーザー情報を取得
+    user_info_url = "https://api.twitter.com/1.1/account/verify_credentials.json"
+    user_info = twitter.get(user_info_url).json()
+    
+    # ユーザー情報を処理
+    user = User(id=user_info['id_str'])
+    login_user(user)
+    return redirect(url_for('home'))
+
 # Blueprint登録
 app.register_blueprint(main)
 app.register_blueprint(auth, url_prefix="/auth")  # 認証用のBlueprint
