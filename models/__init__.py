dd5a7674c330eb02c38fcd960364c987bf7335cb
mojitap <ta714kadvance@gmail.com>
2025-02-14 00:30:27 +0900

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,9 @@
-# models パッケージを認識させるためのファイル
-# ここで特定のモデルを一括インポートする場合もあり
-from .user import User
-from .search_history import SearchHistory
+# routes/__init__.py
 
-__all__ = ["User", "SearchHistory"]
+from flask import Blueprint
+# いま「from .auth import auth」のみだが、
+# main.pyのBlueprintオブジェクトも同時にインポートする。
+from .main import main    # 追加
+from .auth import auth
+
+__all__ = ["main", "auth"]
