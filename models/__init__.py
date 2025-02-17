86dec453712c5e762a4df2bf744b49253abcec2b
mojitap <ta714kadvance@gmail.com>
2025-02-17 14:54:41 +0900

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,6 @@
 # ここで特定のモデルを一括インポートする場合もあり
 from .user import User
 from .search_history import SearchHistory
+from .text_evaluation import evaluate_text  # ✅ 追加
 
 __all__ = ["User", "SearchHistory"]
