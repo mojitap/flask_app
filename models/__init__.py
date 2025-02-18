05a3a774c318f2a2cc2e9e45a4ea4e3f9f4d1612
mojitap <ta714kadvance@gmail.com>
2025-02-18 13:32:49 +0900

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
-# models パッケージを認識させるためのファイル
-# ここで特定のモデルを一括インポートする場合もあり
 from .user import User
 from .search_history import SearchHistory
-from .text_evaluation import evaluate_text  # ✅ 追加
+from .text_evaluation import evaluate_text
 
-__all__ = ["User", "SearchHistory"]
+__all__ = ["User", "SearchHistory", "evaluate_text"]
