178ec073171f52b23daeed93f462d839bbf44252
mojitap <ta714kadvance@gmail.com>
2025-02-18 22:44:27 +0900

    Update search_history.py

--- models/search_history.py
+++ models/search_history.py
@@ -3,15 +3,15 @@ from extensions import db
 class SearchHistory(db.Model):
     __tablename__ = 'search_history'
     id = db.Column(db.Integer, primary_key=True)
-    query = db.Column(db.String(255), unique=True, nullable=False, index=True)  # カラム名は query
+    search_query = db.Column(db.String(255), unique=True, nullable=False, index=True)
     count = db.Column(db.Integer, default=1)
 
     @staticmethod
     def add_or_increment(query_text):
-        record = SearchHistory.query.filter_by(query=query_text).first()
+        record = SearchHistory.query.filter_by(search_query=query_text).first()
         if record:
             record.count += 1
         else:
-            record = SearchHistory(query=query_text, count=1)
+            record = SearchHistory(search_query=query_text, count=1)
             db.session.add(record)
         db.session.commit()
