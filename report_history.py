23d066ca5c31b8e08311d567ab14706e63f3a2b5
mojitap <ta714kadvance@gmail.com>
2025-03-18 11:57:40 +0900

    Add files via upload

---
+++ report_history.py
@@ -0,0 +1,15 @@
+# models/report_history.py
+
+from extensions import db
+from datetime import datetime
+
+class ReportHistory(db.Model):
+    __tablename__ = "report_history"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    text_content = db.Column(db.String(500), nullable=False)
+    judgement = db.Column(db.String(50), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # 必要なら user_id, ip_address など追加してもOK
+    # e.g. user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
